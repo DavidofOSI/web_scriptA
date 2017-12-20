1564aa2f5af5f7c0282d1304016a2bda29bc7821
unknown <ADMIN@WIN10LR1253>
2017-12-20 15:27:54 -0500

    take thinktimeworng

--- Action.c
+++ Action.c
@@ -4,6 +4,9 @@ lr_start_transaction("01_test");
 lr_thinktime(30);
 lr_end_transaction("01_test", LR_AUTO);
 
+lr_start_transaction("02_test");
+lr_thinktime(30);
+lr_end_transaction("02_test", LR_AUTO);
 
 
 	return 0;
