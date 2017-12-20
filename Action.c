23b2ff7d0ac34088e32e9d0d938627100a791d49
unknown <ADMIN@WIN10LR1253>
2017-12-20 18:21:55 -0500

    web acriptA

--- Action.c
+++ Action.c
@@ -1,12 +1,16 @@
 Action()
 {
 lr_start_transaction("01_test");
-lr_thinktime(30);
+lr_think_time(3);
 lr_end_transaction("01_test", LR_AUTO);
 
 lr_start_transaction("02_test");
-lr_thinktime(30);
+lr_think_time(3);
 lr_end_transaction("02_test", LR_AUTO);
+
+lr_start_transaction("03_test");
+lr_think_time(3);  //added at 18:21
+lr_end_transaction("03_test", LR_AUTO);
 
 
 	return 0;
